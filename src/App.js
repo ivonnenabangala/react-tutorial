c730fe1f0005652c59fc16d3f89f4896a519706c
ivonnenabangala <ivonnenabangala@gmail.com>
2023-12-10 16:32:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from "react"
+import Table from './Table'
+class App extends Component{
+    render() {
+        const characters = [
+            {
+              name: 'Charlie',
+              job: 'nurse'
+            },
+            {
+                name: 'Mac',
+                job: 'doctor'
+            },
+            {
+                name: 'Doe',
+                job: 'Actress'
+            },
+            {
+                name: 'Dennis',
+                job: 'teacher'
+            }
+        ]
+      return( 
+        <div className="container">
+        <Table charData={characters}/>
+      </div>
+      )
+    }
+  }
+  export default App
