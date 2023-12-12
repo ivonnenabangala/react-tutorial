c1160983588e6d12cd357ee7c52916a8fb2242e2
ivonnenabangala <ivonnenabangala@gmail.com>
2023-12-12 10:25:49 +0300

    react state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React, { Component } from "react"
 import Table from './Table'
 class App extends Component{
-    render() {
-        const characters = [
-            {
-              name: 'Charlie',
-              job: 'nurse'
-            },
-            {
-                name: 'Mac',
-                job: 'doctor'
-            },
-            {
-                name: 'Doe',
-                job: 'Actress'
-            },
-            {
-                name: 'Dennis',
-                job: 'teacher'
-            }
-        ]
-      return( 
-        <div className="container">
-        <Table charData={characters}/>
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'nurse'
+      },
+      {
+          name: 'Mac',
+          job: 'doctor'
+      },
+      {
+          name: 'Doe',
+          job: 'Actress'
+      },
+      {
+          name: 'Dennis',
+          job: 'teacher'
+      }
+    ]
+  }
+  removeCharacter = (index) => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+  render() {
+    const { characters } = this.state
+  
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
-      )
-    }
+    )
+  }
   }
   export default App
